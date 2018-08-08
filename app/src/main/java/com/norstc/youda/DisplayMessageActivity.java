db9e79e78d37f8da4277d21508a36db7f65eb1fc
norstc <norstc@gmail.com>
2018-08-08 15:30:56 +0800

    logcat

--- app/src/main/java/com/norstc/youda/DisplayMessageActivity.java
+++ app/src/main/java/com/norstc/youda/DisplayMessageActivity.java
@@ -3,15 +3,20 @@ package com.norstc.youda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
+        //手机要打开日志输出 魅族note5 =》设置 =》辅助功能=》开发者选项 =》性能优化=》高级日志输出=》全部允许
+        Log.d("DisplayMessageActivity","hello message" );
+        Log.i("some info","what");
         // Get intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
--- app/src/main/java/com/norstc/youda/MainActivity.java
+++ app/src/main/java/com/norstc/youda/MainActivity.java
@@ -3,6 +3,7 @@ package com.norstc.youda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity","hello world" );
     }
 
     public void sendMessage(View view){
